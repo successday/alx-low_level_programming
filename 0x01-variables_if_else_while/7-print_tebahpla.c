234edc0456c9d7c875e5050a0941d87e941bb0f7
khedidja <successnewday@gmail.com>
2023-06-16 15:15:15 -0700

    print7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print 0-9 using putcher while using int variable
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char digit = 'z';
+while (ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
+
