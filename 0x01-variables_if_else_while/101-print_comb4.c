50415068fc5962a04c008e90a489150ffc5a3132
khedidja <successnewday@gmail.com>
2023-06-16 17:17:57 -0700

    p11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,10 +15,7 @@ while (digit2 <= 9)
 digit3 = 0;
 while (digit3 <= 9)
 {
-if (digit1 != digit2 &&
-digit1 < digit2 &&
-digit2 != digit3 &&
-digit2 < digit3)
+if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
 {
 putchar(digit1 + 48);
 putchar(digit2 + 48);
