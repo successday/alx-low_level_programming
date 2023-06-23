fa2dca3ec28a3d23bb1423cdcebc8cd54a8a5309
khedidja <successnewday@gmail.com>
2023-06-23 03:23:33 -0700

    8ps

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,16 +5,24 @@
  *
  * @size: is the size of square
  *
- * Return: Always 0 (Success)
  *
 */
 void print_square(int size)
 {
-int row, column;
-for (row = 1; row <= size; row++)
+if (size <= 0)
 {
-for (column = 1; column <= size; column++)
-_putchar('#');
 _putchar('\n');
+}
+else
+{
+int row, column;
+for (row = 1; row < size; row++)
+{
+for (column = 1; column < size; column++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
 }
