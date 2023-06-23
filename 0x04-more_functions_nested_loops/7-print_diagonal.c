444e8a260b9dec9d9ede66138fe97fa90e5f8d4f
khedidja <successnewday@gmail.com>
2023-06-23 02:59:57 -0700

    7pr

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,22 @@
 */
 void print_diagonal(int n)
 {
-int postn, space;
 if (n <= 0)
+{
 _putchar('\n');
+}
 else
 {
-for (postn = 1; postn <= n; postn++)
+int postn, space;
+for (postn = 0; postn < n; postn++)
 {
-for (space = 1; space <= postn; space++)
+for (space = 0; space < n; space++)
+{
+if (space = postn)
+_putchar('\\');
+else if (postn < space)
 _putchar(' ');
-_putchar(92); /* is equal to '/' char*/
+}
 _putchar('\n');
 }
 }
