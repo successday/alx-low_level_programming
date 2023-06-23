dc02754322d5452852fed3a2bdd66c947b2f7d07
khedidja <successnewday@gmail.com>
2023-06-23 16:02:37 -0700

    plc

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,21 +3,18 @@
 /**
  * print_line - draws a straight line 
  *
- * @n: number of times the character _ should be printed
+ * @n: number of times the character _  character
+ * should be printed
 */
 void print_line(int n)
 {
+int lnchr;
 if (n <= 0)
-{
 _putchar('\n');
-}
 else
 {
-int j;
-for (j = 1; j <= n; j++)
-{
+for (lnchr = 1; lnchr <= n; lnchr++)
 _putchar('_');
-}
 _putchar('\n');
 }
 }
