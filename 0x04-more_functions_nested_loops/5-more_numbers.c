a592c2c122b063f7c099fa7ec66fae64d0013639
khedidja <successnewday@gmail.com>
2023-06-23 03:18:23 -0700

    5more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,14 @@
 void more_numbers(void)
 {
 int row, count;
-for (row = 0; row <= 10; row++)
+for (row = 0; row < 10; row++)
 {
-for (count = 0; count <= 15; count++)
+for (count = 0; count < 15; count++)
 {
 if (count >= 10)
-_putchar(count / 10 + 48);
-_putchar(count % 10 + 48);
+_putchar(count / 10 + '0');
+_putchar(count % 10 + '0');
 }
--putchar('\n');
+_putchar('\n');
 }
 }
