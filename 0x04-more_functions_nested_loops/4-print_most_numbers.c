da03bf74f19df6f86aa712944c552a21c57505d2
khedidja <successnewday@gmail.com>
2023-06-23 01:46:51 -0700

    4print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 int num:
 for (num = 0; num <= 9; num++)
 {
-if (num == 2 || num == 4)
+if (num !=2 &&  num != 4)
 continue;
-_putchar(num + 48)
+_putchar(num + '0')
 }
 -putchar('\n');
 }
